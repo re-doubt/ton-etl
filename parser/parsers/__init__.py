759afe0dc6bb67a863f5ef5207d70a62f89cc2ef
Pavel Shuvalov <pavel.s@ton.org>
2024-09-18 08:51:52 +0400

    Fix Storm NOT Vault address

--- parser/parsers/__init__.py
+++ parser/parsers/__init__.py
@@ -27,9 +27,9 @@ _parsers = [
     # TON Vault
     CorePricesStormTrade(EMULATOR_PATH, Parser.uf2raw('EQDpJnZP89Jyxz3euDaXXFUhwCWtaOeRmiUJTi3jGYgF8fnj'),
                           Parser.uf2raw('EQCNY2AQ3ZDYwJAqx_nzl9i9Xhd_Ex7izKJM6JTxXRnO6n1F')),
-    # TON Vault
+    # NOT Vault
     CorePricesStormTrade(EMULATOR_PATH, Parser.uf2raw('EQAG8_BzwlWkmqb9zImr9RJjjgZZCLMOQXP9PR0B1PYHvfSS'),
-                         Parser.uf2raw('EQCNY2AQ3ZDYwJAqx_nzl9i9Xhd_Ex7izKJM6JTxXRnO6n1F')),
+                         Parser.uf2raw('EQAqtjTPy9vjuXUDaR4HgMPQfJBpIQorJLmRuk-ZQbRT-eiY')),
     # USDT Vault
     CorePricesStormTrade(EMULATOR_PATH, Parser.uf2raw('EQAz6ehNfL7_8NI7OVh1Qg46HsuC4kFpK-icfqK9J3Frd6CJ'),
                          Parser.uf2raw('EQCup4xxCulCcNwmOocM9HtDYPU8xe0449tQLp6a-5BLEegW')),
