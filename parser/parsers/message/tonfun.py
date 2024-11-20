35e9159879a5859b43cb185efa79d8d57f00901c
Pavel Shuvalov <pavel.s@ton.org>
2024-11-20 18:08:47 +0400

    fixes for referral parser

--- parser/parsers/message/tonfun.py
+++ parser/parsers/message/tonfun.py
@@ -2,26 +2,30 @@ from typing import Optional
 from functools import partial
 from operator import eq
 from decimal import Decimal
-from pytoniq_core import Cell
+from loguru import logger
+from pytoniq_core import Cell, Slice
 from model.parser import Parser, TOPIC_MESSAGES
 from model.tonfun import TonFunTradeEvent
 from db import DB
 
-BCL_MASTER = "EQDrB5FAongX_u-eWu7sHPv0knlnxfidzIxn5Q_ZC40loyDa"
+# BCL_MASTER = "EQDrB5FAongX_u-eWu7sHPv0knlnxfidzIxn5Q_ZC40loyDa"
 is_event = partial(eq, "out").__call__
 
 EVENT_TYPES = {
-    0x8e911e50: "buy_log",
-    0xc8bd8835: "sell_log",
-    0x4a35b2df: "send_liq_log"
+    Parser.opcode_signed(0xcd78325d): "buy_log",
+    Parser.opcode_signed(0x5e97d116): "sell_log",
+    Parser.opcode_signed(0x0f6ab54f): "send_liq_log"
 }
 
-def parse_referral(cs: Cell) -> dict:
+def parse_referral(cs: Slice) -> dict:
+    opcode = cs.load_uint(32) # crc32(ref_v1)
+    if opcode != 0xf7ecea4c:
+        logger.warning(f"Unknown referral opcode: {opcode}")
+        return {}
     return {
-        "referral": cs.to_string(),
-        "partner_address": str(cs.load_address()) if cs.load_bit() else None,
-        "platform_tag": str(cs.load_address()) if cs.load_bit() else None,
-        "extra_tag": str(cs.load_address()) if cs.load_bit() else None
+        "partner_address": cs.load_address(),
+        "platform_tag": cs.load_address() if cs.remaining_bits else None,
+        "extra_tag": cs.load_address() if cs.remaining_bits else None,
     }
 
 def parse_event(cs: Cell) -> Optional[dict]:
@@ -50,9 +54,9 @@ def make_event(obj: dict, trade_data: dict) -> TonFunTradeEvent:
         event_time=obj["created_at"],
         bcl_master=obj["source"],
         event_type=trade_data["type"],
-        trader_address=str(trade_data["trader"]),
-        ton_amount=to_decimal(trade_data["ton_amount"]),
-        bcl_amount=to_decimal(trade_data["bcl_amount"]),
+        trader_address=trade_data["trader"],
+        ton_amount=int(trade_data["ton_amount"]),
+        bcl_amount=int(trade_data["bcl_amount"]),
         min_receive=None,
         referral=trade_data["referral"],
         partner_address=trade_data["partner_address"],
@@ -63,13 +67,17 @@ def make_event(obj: dict, trade_data: dict) -> TonFunTradeEvent:
 class TonFunTrade(Parser):
     topics = lambda _: [TOPIC_MESSAGES]
 
+    # ext out with specific opcodes
     predicate = lambda _, obj: (
         obj.get("opcode") in EVENT_TYPES and
-        is_event(obj.get("direction")) and
-        obj.get("source", "").startswith(BCL_MASTER)  # Проверяем, что источник - мастер контракт
+        obj.get("direction") == "out" and 
+        obj.get("destination", 'None') is None
     )
 
     def handle_internal(self, obj: dict, db: DB) -> None:
+        # check 
         maybe_trade_data = parse_event(Parser.message_body(obj, db).begin_parse())
         if maybe_trade_data:
-            db.serialize(make_event(obj, maybe_trade_data))
+            event = make_event(obj, maybe_trade_data)
+            logger.info(f"Parsed tonfun event: {event}")
+            db.serialize(event)
