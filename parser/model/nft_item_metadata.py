ab75859d62359d3ce2791b94fd42a402ea7a514b
Aleksandr Kaekhtin <kaehtin.aa@gmail.com>
2025-01-20 18:26:30 +0300

    Add NFT item metadata parser

---
+++ parser/model/nft_item_metadata.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class NFTItemMetadata:
+    __tablename__ = "nft_item_metadata"
+    __schema__ = "parsed"
+
+    address: str
+    update_time_onchain: int
+    update_time_metadata: int = None
+    content: str
+    metadata_status: int = 0
+    name: str = None
+    description: str = None
+    attributes: str = None
+    image: str = None
+    image_data: str = None
+    sources: str = None
+    tonapi_image_url: str = None
